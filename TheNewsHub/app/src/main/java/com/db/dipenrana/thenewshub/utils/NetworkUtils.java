f5469fbaf5d5c82e33d5486ac24d7ae0c95b8d58
drana <dipen.rana@hotmail.com>
2017-09-19 07:18:26 -0700

    changed to basic activity

---
+++ TheNewsHub/app/src/main/java/com/db/dipenrana/thenewshub/utils/NetworkUtils.java
@@ -0,0 +1,11 @@
+package com.db.dipenrana.thenewshub.utils;
+
+/**
+ * Created by dipenrana on 9/18/17.
+ */
+
+public class NetworkUtils {
+
+    public static final String NYT_API_KEY = "3ae9d158e4744dfb85debb2906d27b77";
+    public static final String API_URL = "";
+}
