8038ba7eb81461fc5ca864af47626a8f6dde390a
drana <dipen.rana@hotmail.com>
2017-09-20 00:16:14 -0700

    gson to json

---
+++ TheNewsHub/app/src/main/java/com/db/dipenrana/thenewshub/activities/ArticleItem.java
@@ -0,0 +1,15 @@
+package com.db.dipenrana.thenewshub.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.db.dipenrana.thenewshub.R;
+
+public class ArticleItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_item);
+    }
+}
